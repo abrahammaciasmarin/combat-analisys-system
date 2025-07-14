a03fc473e393fa40aeeaf0deb9eb653b82aa696c
abrahammaciasmarin <abrahammacias755@gmail.com>
2025-07-14 17:24:31 -0600

    video parser service

---
+++ video-parser-service/src/main/java/com/abraham/parser/video_parser_service/VideoParserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.abraham.parser.video_parser_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoParserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoParserServiceApplication.class, args);
+	}
+
+}
---
+++ video-parser-service/src/test/java/com/abraham/parser/video_parser_service/VideoParserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abraham.parser.video_parser_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoParserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
